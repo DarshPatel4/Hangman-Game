07a484a7ad6de6e9910a69e7248da2eb1a5abd36
Darsh Patel <jayjalaram25104@gmail.com>
2024-04-08 13:51:29 +0530

    changes

--- hangman.py
+++ hangman.py
@@ -1,7 +1,8 @@
 import random
+import string
 
 # List of words for the game
-words = ["apple", "banana", "orange", "grape", "strawberry", "watermelon", "pineapple"]
+words = ["apple", "banana", "orange", "grape", "strawberry", "watermelon", "pineapple", "kiwi", "peach", "mango", "lemon", "cherry", "blueberry", "raspberry"]
 
 # Function to choose a random word from the list
 def choose_word():
@@ -21,6 +22,7 @@ def display_word(word, guessed_letters):
 def main():
     print("Welcome to Hangman!")
     word = choose_word()
+    print("The word has", len(word), "letters.")
     guessed_letters = []
     attempts = 6
 
@@ -28,12 +30,17 @@ def main():
         print("\nAttempts left:", attempts)
         displayed_word = display_word(word, guessed_letters)
         print("Word:", displayed_word)
+        print("Guessed letters:", ', '.join(guessed_letters))
 
         if "_" not in displayed_word:
             print("Congratulations! You guessed the word:", word)
             break
 
         guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or guess not in string.ascii_lowercase:
+            print("Please enter a single letter.")
+            continue
 
         if guess in guessed_letters:
             print("You already guessed that letter. Try again.")
@@ -43,7 +50,7 @@ def main():
 
         if guess not in word:
             attempts -= 1
-            print("Incorrect guess!")
+            print("Incorrect guess! Try again.")
 
         if attempts == 0:
             print("Out of attempts! The word was:", word)
@@ -52,36 +59,3 @@ def main():
 # Call the main function
 if __name__ == "__main__":
     main()
-
-
-# import time
-
-# def calculate_typing_speed(text, elapsed_time):
-#     words = text.split()
-#     num_words = len(words)
-#     minutes = elapsed_time / 60
-#     wpm = num_words / minutes
-#     return wpm
-
-# def main():
-#     print("Welcome to the Typing Speed Tester!")
-#     print("Type the following text as fast as you can:")
-
-#     # Text to be typed
-#     text = "The quick brown fox jumps over the lazy dog."
-#     print(text)
-    
-#     input("Press Enter when you are ready to start typing...")
-    
-#     start_time = time.time()
-#     user_input = input("Start typing: ")
-#     end_time = time.time()
-
-#     elapsed_time = end_time - start_time
-#     typing_speed = calculate_typing_speed(text, elapsed_time)
-    
-#     print("\nYou typed the text in {:.2f} seconds.".format(elapsed_time))
-#     print("Your typing speed is {:.2f} words per minute (wpm).".format(typing_speed))
-
-# if __name__ == "__main__":
-#     main()
